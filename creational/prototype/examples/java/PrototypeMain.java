6fa57c201a94d6908cbcb9c770ba273015885b05
Purvansh Parmar <purvanshparmar15@gmail.com>
2022-07-24 08:02:27 +0530

    Completed prototype example

--- creational/prototype/examples/java/PrototypeMain.java
+++ creational/prototype/examples/java/PrototypeMain.java
@@ -1,3 +1,13 @@
 public class PrototypeMain {
-    
+    public static void main(String[] args) {
+        Paint paint = new Line(0, 0, 10, 10);
+        paint.display();
+        Paint paint2 = paint.clone();
+        paint2.display();
+        Paint paint3 = new Square(0, 0, 10, 10);
+        paint3.display();
+        Paint paint4 = paint3.clone();
+        paint4.display();
+        
+    }
 }
